82b583be51a3f038ff6fda2499351e76bc1c62b6
prodject <anonymous.prodject@gmail.com>
2020-04-03 20:10:54 +0300

    Fix error. Installing package <apt-parrot> replace new repo.

--- parrot-wsl.sh
+++ parrot-wsl.sh
@@ -38,10 +38,12 @@ function core_install() {
   echo "#deb-src https://deb.parrotlinux.org/parrot/ rolling main contrib non-free" >> /etc/apt/sources.list.d/parrot.list
   echo "deb https://deb.parrotlinux.org/parrot/ rolling-security main contrib non-free" >> /etc/apt/sources.list.d/parrot.list
   echo "#deb-src https://deb.parrotlinux.org/parrot/ rolling-security main contrib non-free" >> /etc/apt/sources.list.d/parrot.list
+  cp -f /etc/apt/sources.list.d/parrot.list /etc/apt/sources.list.d/parrot.list.c
 	echo -e "# This file is empty, feel free to add here your custom APT repositories\n\n# The standard Parrot repositories are NOT here. If you want to\n# edit them, take a look into\n#                      /etc/apt/sources.list.d/parrot.list\n#                      /etc/apt/sources.list.d/debian.list\n\n\n\n# If you want to change the default parrot repositories setting\n# another localized mirror, then use the command parrot-mirror-selector\n# and see its usage message to know what mirrors are available\n\n\n\n#uncomment the following line to enable the Parrot Testing Repository\n#deb http://us.repository.frozenbox.org/parrot testing main contrib nonfree" > /etc/apt/sources.list
 	wget --no-check-certificate -qO - https://archive.parrotsec.org/parrot/misc/parrotsec.gpg | apt-key add -
 	apt-get update
 	apt-get -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-overwrite" install apt-parrot
+	cp -f /etc/apt/sources.list.d/parrot.list.c /etc/apt/sources.list.d/parrot.list
 	apt-get update
 	apt-get -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-overwrite" install parrot-core
 	apt-get -y --allow-downgrades --allow-remove-essential --allow-change-held-packages -o Dpkg::Options::="--force-overwrite" dist-upgrade
